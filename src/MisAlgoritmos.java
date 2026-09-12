25ca33b06128c9335ec1e2f36bb2dfcc0bc9299f
anfacoma1a-collab <anfacoma1a@gmail.com>
2026-09-11 22:33:12 -0500

    Working Hard

--- src/MisAlgoritmos.java
+++ src/MisAlgoritmos.java
@@ -78,11 +78,6 @@ public class MisAlgoritmos implements Ordenamientos,Busquedas{
 
     @Override
     public int[] insertionSort(int[] arrayDesordenado) {
-        return new int[0];
-    }
-
-    @Override
-    public int[] mergeSort(int[] arrayDesordenado) {
 
         for (int i = 1; i < arrayDesordenado.length; i++) {
             int elementoActual = arrayDesordenado[i];
@@ -96,10 +91,73 @@ public class MisAlgoritmos implements Ordenamientos,Busquedas{
 
                 posicionAnterior--;
             }
+
             arrayDesordenado[posicionAnterior + 1] = elementoActual;
         }
+
         return arrayDesordenado;
+    }
+
+    @Override
+    public int[] mergeSort(int[] arrayDesordenado) {
+        if (arrayDesordenado.length <= 1) {
+            return arrayDesordenado;
+        }
+
+        int mitad = arrayDesordenado.length / 2;
+
+        int[] izquierda = new int[mitad];
+        int[] derecha = new int[arrayDesordenado.length - mitad];
+
+        for (int i = 0; i < mitad; i++) {
+            izquierda[i] = arrayDesordenado[i];
+        }
+
+        for (int i = mitad; i < arrayDesordenado.length; i++) {
+            derecha[i - mitad] = arrayDesordenado[i];
+        }
+
+        izquierda = mergeSort(izquierda);
+        derecha = mergeSort(derecha);
+
+        return unir(izquierda, derecha);
+    }
+
+    public int[] unir(int[] izquierda, int[] derecha) {
+        int[] resultado = new int[izquierda.length + derecha.length];
+
+        int posicionIzquierda = 0;
+        int posicionDerecha = 0;
+        int posicionResultado = 0;
+
+        while (posicionIzquierda < izquierda.length
+                && posicionDerecha < derecha.length) {
+
+            if (izquierda[posicionIzquierda] < derecha[posicionDerecha]) {
+                resultado[posicionResultado] = izquierda[posicionIzquierda];
+                posicionIzquierda++;
+            } else {
+                resultado[posicionResultado] = derecha[posicionDerecha];
+                posicionDerecha++;
             }
+
+            posicionResultado++;
+        }
+
+        while (posicionIzquierda < izquierda.length) {
+            resultado[posicionResultado] = izquierda[posicionIzquierda];
+            posicionIzquierda++;
+            posicionResultado++;
+        }
+
+        while (posicionDerecha < derecha.length) {
+            resultado[posicionResultado] = derecha[posicionDerecha];
+            posicionDerecha++;
+            posicionResultado++;
+        }
+
+        return resultado;
+    }
 
     @Override
     public int[] quickSort(int[] arrayDesordenado) {
